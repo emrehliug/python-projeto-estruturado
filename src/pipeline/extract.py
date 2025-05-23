733f9f519504ed23b0f814a7c3d36b605f666bc1
emrehliug <guigui410@gmail.com>
2025-05-23 19:23:25 -0300

    finalizado metodo de extração de arquivos por diretorio e com 3 tipos de extensao

--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -18,30 +18,38 @@ def extract(path: str) -> List[pd.DataFrame]:
     try:
         # Verifica se o diretório existe
         if not os.path.exists(path):
-            raise FileNotFoundError(f"Directory {path} does not exist.")
+            raise FileNotFoundError(f"Diretorio {path} não existe.")
 
-        # Verifica se o tipo de arquivo é válido
-        if file_type not in ['csv', 'json', 'xlsx']:
-            raise ValueError("file_type must be 'csv' or 'json'")
+        get_filespath_in_dir = os.listdir(path)
+        if(len(set(get_filespath_in_dir)) == 0):
+            raise ValueError("O diretório não contém arquivos. Verifique se o diretório está vazio.")
+        
+        list_file_types = []
+        
+        for file in get_filespath_in_dir:
+            list_file_types.append(file.split(".")[-1])
 
-        # Define o tipo de arquivo
-        file_type = os.path.splitext(path)[1]
-
-        # Cria uma lista com os arquivos do diretório
-        files = glob.glob(os.path.join(path, f"*.{file_type}"))
-
+        distinct_file_types = set(list_file_types)
+        
         # Cria uma lista para armazenar os dataframes
         df_list = []
+        
+        for file_type in distinct_file_types:
+            if file_type not in ['csv', 'json', 'xlsx']:
+                raise ValueError("O diretório contém arquivo do tipo invalido. Os tipos válidos são: csv, json e xlsx.")
+            
+            # Cria uma lista com os arquivos do diretório que possuem o tipo de arquivo desejado
+            files = glob.glob(os.path.join(path, f"*.{file_type}"))
 
-        # Loop para ler os arquivos e enviar para o dataframe
-        for file in files:
-            if file_type == 'csv':
-                df = pd.read_csv(file)
-            elif file_type == 'json':
-                df = pd.read_json(file)
-            elif file_type == 'xlsx':
-                df = pd.read_excel(file)
-            df_list.append(df)
+            # Loop para ler os arquivos e enviar para o dataframe
+            for file in files:
+                if file_type == 'csv':
+                    df = pd.read_csv(file)
+                elif file_type == 'json':
+                    df = pd.read_json(file)
+                elif file_type == 'xlsx':
+                    df = pd.read_excel(file)
+                df_list.append(df)
 
         return df_list
     
@@ -51,7 +59,8 @@ def extract(path: str) -> List[pd.DataFrame]:
 
 if __name__ == "__main__":
     # Testa a função
-    path = "../data/input/"
+    path = r"C:\Users\gliminha\Desktop\Workspace\python-projeto-estruturado\data\input"
+    
     df_list = extract(path)
     for df in df_list:
         print(df.head())
